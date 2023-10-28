85a88ec8d8ff00ade88f4f37fa203b3bfc3f1b3e
MatiasMoore <nikita.birukov.03@mail.ru>
2023-10-28 09:46:26 +0300

    Level editor and InputSystem prototypes

---
+++ Assets/Scripts/LevelEditor.cs
@@ -0,0 +1,150 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LevelEditor : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject canvas;
+
+    [SerializeField]
+    private GameObject parent;
+
+    [SerializeField]
+    private GameObject wallPrefab;
+
+    [SerializeField]
+    private GameObject pathPrefab;
+
+    [SerializeField]
+    private GameObject enemyPrefab;
+
+    [SerializeField]
+    private GameObject bonusPrefab;
+
+    [SerializeField]
+    private GameObject checkpointPrefab;
+
+    private PlayerInput playerInput;
+
+    //Buttons
+    private InputAction addWallAction;
+    private InputAction addPathAction;
+    private InputAction addEnemyAction;
+    private InputAction addBonusAction;
+    private InputAction addCheckpointAction;
+
+    //Positions
+    private InputAction mousePositionAction;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+
+        addWallAction = playerInput.actions["AddWall"];
+        addPathAction = playerInput.actions["AddPath"];
+        addEnemyAction = playerInput.actions["AddEnemy"];
+        addBonusAction = playerInput.actions["AddBonus"];
+        addCheckpointAction = playerInput.actions["AddCheckpoint"];
+
+        mousePositionAction = playerInput.actions["MousePosition"];
+    }
+
+    private void OnEnable()
+    {
+        addWallAction.performed += addWall;
+        addPathAction.performed += addPath;
+        addEnemyAction.performed += addEnemy;
+        addBonusAction.performed += addBonus;
+        addCheckpointAction.performed += addCheckpoint;
+        Debug.Log("Enabled editor!");
+    }
+
+    private void OnDisable()
+    {
+        addWallAction.performed -= addWall;
+        addPathAction.performed -= addPath;
+        addEnemyAction.performed -= addEnemy;
+        addBonusAction.performed -= addBonus;
+        addCheckpointAction.performed -= addCheckpoint;
+        Debug.Log("Disabled editor!");
+    }
+
+    private Vector3 getMousePosAllignedWithGrid()
+    {
+        Vector3 mousePos = mousePositionAction.ReadValue<Vector2>();
+        mousePos.z = 3 - Camera.main.transform.position.z;
+
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        worldPos.z = -1;
+
+        return new Vector3((int)Mathf.Round(worldPos.x), (int)Mathf.Round(worldPos.y), (int)Mathf.Round(worldPos.z));
+    }
+
+    private GameObject getObjectAtPosition(Vector3 mousePos)
+    {
+        Vector3 raycastOrigin = mousePos;
+        Vector3 raycastDir = mousePos - raycastOrigin;
+        RaycastHit2D hit = Physics2D.Raycast(raycastOrigin, raycastDir);
+        if (hit)
+        {
+            if (hit.transform.gameObject == this.canvas)
+                return null;
+            else
+            {
+                Debug.Log("hit prefab");
+                return hit.transform.gameObject;
+            }
+        }
+        else
+        {
+            return null;
+            Debug.Log("Raycast missed");
+        }
+    }
+
+    private void addWall(InputAction.CallbackContext context)
+    {
+        Vector3 mouseGridPos = getMousePosAllignedWithGrid();
+        GameObject prevObject = getObjectAtPosition(mouseGridPos);
+        if (prevObject == null || prevObject.GetPrefabDefinition() == pathPrefab.GetPrefabDefinition())
+        {
+            if (prevObject != null)
+                Destroy(prevObject);
+            Instantiate(wallPrefab, mouseGridPos, Quaternion.identity, parent.transform);
+            Debug.Log("Added wall");
+        }
+    }
+
+    private void addPath(InputAction.CallbackContext context)
+    {
+        Vector3 mouseGridPos = getMousePosAllignedWithGrid();
+        if (getObjectAtPosition(mouseGridPos) == null)
+        {
+            Instantiate(pathPrefab, mouseGridPos, Quaternion.identity, parent.transform);
+            Debug.Log("Added path");
+        }
+    }
+
+    private void addEnemy(InputAction.CallbackContext context)
+    {
+        Debug.Log("Added enemy");
+
+    }
+
+    private void addBonus(InputAction.CallbackContext context)
+    {
+        Debug.Log("Added bonus");
+
+    }
+
+    private void addCheckpoint(InputAction.CallbackContext context)
+    {
+        Debug.Log("Added checkpoint");
+
+    }
+
+}
---
+++ Assets/Scripts/TouchManager.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TouchManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private GameObject mazeParent;
+
+    [SerializeField]
+    private GameObject wallPrefab;
+
+    private PlayerInput playerInput;
+
+    private InputAction touchPositionAction;
+    private InputAction touchPressAction;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        touchPressAction = playerInput.actions["TouchPress"];
+        touchPositionAction = playerInput.actions["TouchPosition"];
+    }
+
+    private void OnEnable()
+    {
+        touchPressAction.performed += touchPressed;
+        Debug.Log("Enabled!");
+    }
+
+    private void OnDisable()
+    {
+        touchPressAction.performed -= touchPressed;
+
+        string localPath = "Assets/Prefabs/" + "Level" + ".prefab";
+        bool ok;
+        PrefabUtility.SaveAsPrefabAsset(mazeParent, localPath, out ok);
+        if (ok)
+            Debug.Log("Saved!");
+        else
+            Debug.Log("Failed to save!");
+
+        Debug.Log("Disabled!");
+    }
+
+    private Vector3 lastPos;
+
+    private void touchPressed(InputAction.CallbackContext context)
+    {
+        //float value = context.ReadValue<fl/oat>();
+        //Debug.Log(value);
+
+        Vector3 touchPos = touchPositionAction.ReadValue<Vector2>();
+        Debug.Log(touchPos);
+        /**/
+        touchPos.z = player.transform.position.z - Camera.main.transform.position.z;
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(touchPos);
+
+        //player.transform.position = worldPos;
+
+        Debug.Log(worldPos);
+
+        Vector3 raycastOrigin = worldPos;
+        Vector3 raycastDir = worldPos - raycastOrigin;
+        RaycastHit2D hit = Physics2D.Raycast(raycastOrigin, raycastDir);
+        if (hit)
+        {
+            lastPos = hit.point;
+            Vector3 gridPos = new Vector3((int)Mathf.Round(lastPos.x), (int)Mathf.Round(lastPos.y), (int)Mathf.Round(lastPos.z));
+            Instantiate(wallPrefab, gridPos, Quaternion.identity, mazeParent.transform);
+        }
+        else
+        {
+            Debug.Log("Raycast missed");
+        }
+
+        /**/
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(lastPos, 0.3f);
+    }
+
+    private void touchPosition(InputAction.CallbackContext context)
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
