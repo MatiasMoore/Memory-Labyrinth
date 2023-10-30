f7469c4d895c3d2e862c8a65faa6f9453a8be7d7
Maxim Zverkov <75385902+T-LORE@users.noreply.github.com>
2023-10-30 08:46:40 +0300

    feat(project) added shell of MainCharacter

---
+++ Assets/Scripts/MainCharacter.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MainCharacter : MonoBehaviour
+{
+    public List<Vector2> _debugPath;
+
+    [SerializeField] private float _speed;
+    private ObjectMovement _objectMovement; 
+
+    void Start()
+    {
+        _objectMovement = new ObjectMovement(GetComponent<Transform>(), GetComponent<Rigidbody2D>(), _speed);
+
+        _objectMovement.FollowPath(_debugPath);
+    }
+
+    void FixedUpdate()
+    {
+        _objectMovement.UpdatePosition();
+    }
+
+    public void FollowPath(List<Vector2> path)
+    {
+        _objectMovement.FollowPath(path);
+    }
+
+    public void StopMoving()
+    {
+        _objectMovement.StopMove();
+    }
+}
