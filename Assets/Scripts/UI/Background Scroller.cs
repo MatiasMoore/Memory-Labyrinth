d01ee3ec65702d9f8e5d9b2bb34b3099803b760c
Egor <104233247+RR3Z@users.noreply.github.com>
2023-10-31 20:04:57 +0300

    feat(project): added background scroller script

---
+++ Assets/Scripts/UI/Background Scroller.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] private RawImage _image;
+    [SerializeField] private float _x, _y;
+
+    void Update()
+    {
+        _image.uvRect = new Rect(_image.uvRect.position + new Vector2(_x, _y) * Time.deltaTime, _image.uvRect.size);
+    }
+}
