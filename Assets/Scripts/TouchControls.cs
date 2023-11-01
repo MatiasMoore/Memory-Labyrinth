cba4d3781b076d1c473d36aa9dbe62f00e236ee3
MatiasMoore <nikita.birukov.03@mail.ru>
2023-11-01 19:37:21 +0300

    feat(project): touchControls class now provides touch's world position

--- Assets/Scripts/TouchControls.cs
+++ Assets/Scripts/TouchControls.cs
@@ -7,12 +7,14 @@ using UnityEngine.InputSystem;
 
 public class TouchControls : MonoBehaviour
 {
-
+    //Class with the control map
     private PlayerInput _playerInput;
 
+    //Main actions
     private InputAction _touchPositionAction;
     private InputAction _touchPressAction;
 
+    //Lists of callback functions
     private List<System.Action<InputAction.CallbackContext>> _touchDownCallbacks = new List<System.Action<InputAction.CallbackContext>>();
     private List<System.Action<InputAction.CallbackContext>> _touchUpCallbacks = new List<System.Action<InputAction.CallbackContext>>();
 
@@ -31,6 +33,7 @@ public class TouchControls : MonoBehaviour
 
     private void OnDisable()
     {
+        //Stop all callbacks
         foreach (System.Action<InputAction.CallbackContext> func in _touchDownCallbacks)
         {
             _touchPressAction.performed -= func;
@@ -53,8 +56,17 @@ public class TouchControls : MonoBehaviour
         _touchUpCallbacks.Add(func);
     }
 
-    public Vector2 getTouchPosition()
+    public Vector2 getTouchScreenPosition()
     {
         return _touchPositionAction.ReadValue<Vector2>();
+    }
+
+    public Vector2 getTouchWorldPosition2d()
+    {
+        Vector3 screenCoords = getTouchScreenPosition();
+        screenCoords.z = 5;
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(screenCoords);
+        worldPos.z = 5;
+        return (Vector2)worldPos;
     }
 }
