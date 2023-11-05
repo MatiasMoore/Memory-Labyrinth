eb88d34b1e9fe69fd0f4301a82e600117e298737
Egor <104233247+RR3Z@users.noreply.github.com>
2023-11-05 22:02:55 +0300

    feat(project): added info panel support for phones with "eyebrown"

---
+++ Assets/Scripts/UI/InGame UI/SafeAreaScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class SafeAreaScript : MonoBehaviour
+{
+    private RectTransform _safeAreaRectTransform;
+
+    private void Awake()
+    {
+        _safeAreaRectTransform = GetComponent<RectTransform>();
+        UpdateSafeArea();
+    }
+
+    private void UpdateSafeArea()
+    {
+        Rect safeArea = Screen.safeArea;
+
+        Vector2 bottomLeftAnchor = safeArea.position;
+        Vector2 topRightAnchor = safeArea.position + safeArea.size;
+
+        // Transition from pixels to float in the range [0;1]
+        bottomLeftAnchor.x /= Screen.width;
+        bottomLeftAnchor.y /= Screen.height;
+        topRightAnchor.x /= Screen.width;
+        topRightAnchor.y /= Screen.height;
+
+        _safeAreaRectTransform.anchorMin = bottomLeftAnchor;
+        _safeAreaRectTransform.anchorMax = topRightAnchor;
+    }
+}
