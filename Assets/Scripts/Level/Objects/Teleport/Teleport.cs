ab64f5ae77c14bffb9c0d8e159aac6dd126657f1
Maxim Zverkov <75385902+T-LORE@users.noreply.github.com>
2023-11-12 21:02:46 +0300

    feat(project): add showing teleport position

--- Assets/Scripts/Level/Objects/Teleport/Teleport.cs
+++ Assets/Scripts/Level/Objects/Teleport/Teleport.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(LineRenderer))]
 public class Teleport : MonoBehaviour
 {
     [SerializeField]
@@ -11,6 +12,15 @@ public class Teleport : MonoBehaviour
     public void Start()
     {
         GetComponent<BoxCollider2D>().isTrigger = true;
+
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        float thickness = 0.3f;
+        lineRenderer.startWidth = thickness;
+        lineRenderer.endWidth = thickness;
+
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, _target.position);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -25,5 +35,10 @@ public class Teleport : MonoBehaviour
         {
             Debug.Log($"Teleporting {other.gameObject.name} failed");
         }
+    }
+
+    public void DisableTeleportPath()
+    {
+        GetComponent<LineRenderer>().enabled = false;
     }
 }
