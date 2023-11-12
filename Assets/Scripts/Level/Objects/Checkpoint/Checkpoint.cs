af3d2d1fd8dba51c30f812cd5d01a0d30c6e1261
Maxim Zverkov <75385902+T-LORE@users.noreply.github.com>
2023-11-12 20:49:40 +0300

    fix(project): fixed multiple checkpoint achievement

--- Assets/Scripts/Level/Objects/Checkpoint/Checkpoint.cs
+++ Assets/Scripts/Level/Objects/Checkpoint/Checkpoint.cs
@@ -8,6 +8,8 @@ public class Checkpoint : MonoBehaviour
     [SerializeField]
     private int _queue;
 
+    private bool _isReached = false;
+
     public void Start()
     {
         GetComponent<BoxCollider2D>().isTrigger = true;
@@ -19,20 +21,22 @@ public class Checkpoint : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D other)
-    {
-        CheckpointCompatable checkpointObject =
-            other.gameObject.GetComponent<CheckpointCompatable>();
-        if (checkpointObject != null)
+    {   if (!_isReached)
         {
-            Debug.Log($"Checkpoint {other.gameObject.name} for {_queue}");
-            checkpointObject.getCheckpoint(this);
-
-            // Change color to yellow
-            GetComponent<SpriteRenderer>().color = Color.yellow;
-        }
-        else
-        {
-            Debug.Log($"Checkpoint {other.gameObject.name} failed");
+            CheckpointCompatable checkpointObject =
+                other.gameObject.GetComponent<CheckpointCompatable>();
+            if (checkpointObject != null)
+            {
+                Debug.Log($"Checkpoint {other.gameObject.name} for {_queue}");
+                checkpointObject.getCheckpoint(this);
+                _isReached = true;
+                // Change color to yellow
+                GetComponent<SpriteRenderer>().color = Color.yellow;
+            }
+            else
+            {
+                Debug.Log($"Checkpoint {other.gameObject.name} failed");
+            }
         }
     }
 }
