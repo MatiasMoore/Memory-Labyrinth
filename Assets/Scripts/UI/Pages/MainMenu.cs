00405ddd77c6fd78dfc958d87f5985f53414407d
Egor <104233247+RR3Z@users.noreply.github.com>
2023-11-12 20:29:41 +0300

    feat(project): added event support for menus

--- Assets/Scripts/UI/Pages/MainMenu.cs
+++ Assets/Scripts/UI/Pages/MainMenu.cs
@@ -6,17 +6,23 @@ public class MainMenu : MonoBehaviour
     public void onClickPlay()
     {
         ResourceManager.LoadScene(ResourceManager.AvailableScene.GameField);
+
+        MenuManager.FireButtonClickAction();
     }
 
     public void onClickOptions()
     {
         MenuManager.ClosePage(MenuManager.Page.MAIN);
         MenuManager.OpenPage(MenuManager.Page.OPTIONS);
+
+        MenuManager.FireButtonClickAction();
     }
 
     public void onClickAchievements()
     {
         MenuManager.ClosePage(MenuManager.Page.MAIN);
         MenuManager.OpenPage(MenuManager.Page.ACHIEVEMENTS);
+
+        MenuManager.FireButtonClickAction();
     }
 }
--- Assets/Scripts/UI/Pages/OptionsMenu.cs
+++ Assets/Scripts/UI/Pages/OptionsMenu.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class OptionsMenu : MonoBehaviour
 {
-    public void onClickBack()
+    public void OnClickBack()
     {
         MenuManager.ClosePage(MenuManager.Page.OPTIONS);
         MenuManager.OpenPage(MenuManager.Page.MAIN);
+
+        MenuManager.FireButtonClickAction();
     }
 }
