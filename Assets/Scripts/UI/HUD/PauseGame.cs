e5530abded3df6cbd0d4aaeed789875c15968690
Egor <104233247+RR3Z@users.noreply.github.com>
2023-11-12 21:18:59 +0300

    refactor(project): removed unnecessary log from PauseGame script

--- Assets/Scripts/UI/HUD/PauseGame.cs
+++ Assets/Scripts/UI/HUD/PauseGame.cs
@@ -6,7 +6,6 @@ public class PauseGame : MonoBehaviour
 {
     public void OnClickPause()
     {
-        Debug.Log("PAUSED!");
         MenuManager.OpenPage(MenuManager.Page.PAUSE);
         Time.timeScale = 0f;
         PauseMenu.setPausedGame(true);
