3e831b562e8eb4ac42a15a16bb165f8d93e537a3
Egor <104233247+RR3Z@users.noreply.github.com>
2023-11-13 16:55:32 +0300

    feat(project): added updates for gems count and health on event

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -2,13 +2,24 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
+    private static LevelModel levelModel;
+
+    private static MainCharacter mainCharacter;
+
     public static void SetupListeners()
     {
-        var levelModel = FindObjectOfType<LevelModel>();
+        levelModel = FindObjectOfType<LevelModel>();
         if (levelModel != null)
         {
             levelModel._onLevelWin += ShowWinPanelAction;
             levelModel._onLevelLose += ShowLosePanelAction;
+            levelModel._onPlayerGetBonus += UpdateGemsCountAction;
+        }
+
+        mainCharacter = FindObjectOfType<MainCharacter>();
+        if (mainCharacter != null)
+        {
+            mainCharacter._onDamageEvent += UpdateHealthCountAction;
         }
     }
 
@@ -32,5 +43,24 @@ public class MenuController : MonoBehaviour
         LosePanel losePanel = FindObjectOfType<LosePanel>();
         losePanel.SetLevelTimeOnLose();
         losePanel.SetLevelGemsCountOnLose();
+    }
+
+    private static void UpdateHealthCountAction()
+    {
+        Health health = FindObjectOfType<Health>();
+        if (health != null)
+        {
+            health.SetHealth(mainCharacter.GetHealth());
+        }
+    }
+
+    private static void UpdateGemsCountAction()
+    {
+        Gems gems = FindObjectOfType<Gems>();
+        Debug.Log("MENU CONTROLLER: UpdateGemsCountAction -> " + gems);
+        if (gems != null)
+        {
+            gems.SetGemsAmount(levelModel.GetMoneyAmount());
+        }
     }
 }
