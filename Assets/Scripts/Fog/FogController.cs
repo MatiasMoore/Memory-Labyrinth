0258c76ea0afa3fc8209bba57bc8dcdb30a642cf
MatiasMoore <nikita.birukov.03@mail.ru>
2023-11-13 18:26:59 +0300

    feat(project): added basic fog mechanics

---
+++ Assets/Scripts/Fog/FogController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogController : MonoBehaviour
+{
+    private FogMaskTarget[] _masks = { };
+
+    public void FindTargets()
+    {
+        _masks = FindObjectsOfType<FogMaskTarget>();
+        foreach (var mask in _masks)
+        {
+            mask.CreateMask();
+
+            Vector2 maskPos = mask.GetPos();
+            Vector2 topLeft = (Vector2)Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 5));
+            Vector2 topRight = (Vector2)Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 5));
+            Vector2 bottomRight = (Vector2)Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 5));
+            Vector2 bottomLeft = (Vector2)Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height, 5));
+
+            float toTopLeft = (topLeft - maskPos).magnitude;
+            float toTopRight = (topRight - maskPos).magnitude;
+            float toBottomRight = (bottomRight - maskPos).magnitude;
+            float toBottomLeft = (bottomLeft - maskPos).magnitude;
+
+            float startRadius = Mathf.Max(toTopLeft, toTopRight, toBottomRight, toBottomLeft) * 1.1f;
+            mask.FadeMaskRadius(startRadius, mask.GetPreferredRadius(), 2.5f);
+        }
+    }
+}
