cac59251f5aeeba60e348e04f049394398914918
Egor <104233247+RR3Z@users.noreply.github.com>
2023-11-13 19:07:33 +0300

    feat(project): separated methods for different buttons in the options menu

--- Assets/Scripts/UI/Pages/OptionsMenu.cs
+++ Assets/Scripts/UI/Pages/OptionsMenu.cs
@@ -3,7 +3,17 @@ using UnityEngine.Events;
 
 public class OptionsMenu : MonoBehaviour
 {
-    public void OnClickBack()
+    public void OnClickAccept()
+    {
+        MenuManager.ClosePage(MenuManager.Page.OPTIONS);
+        MenuManager.OpenPage(MenuManager.Page.MAIN);
+
+        // TODO: save audio and music settings
+
+        MenuManager.FireButtonClickAction();
+    }
+
+    public void OnClickDecline()
     {
         MenuManager.ClosePage(MenuManager.Page.OPTIONS);
         MenuManager.OpenPage(MenuManager.Page.MAIN);
