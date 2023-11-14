9eae0da9e4c0fd46b1700d359b4b3bacd8650048
andeysiniy <kakavvmm@gmail.com>
2023-11-14 19:37:13 +0300

    refactor(project): removed unnecessary comments and lines

--- Assets/Scripts/SaveLoad/CurrentLevel.cs
+++ Assets/Scripts/SaveLoad/CurrentLevel.cs
@@ -6,7 +6,6 @@ using static ResourceManager;
 public static class CurrentLevel
 {
     private static LevelData _currentLevel = new LevelData();
-    private static int _currentLevelIndex = -1;
 
     public static void Load(ResourceManager.Level level)
     {
@@ -28,7 +27,6 @@ public static class CurrentLevel
     {
         _currentLevel = newLevelData;
 
-
         if (LevelProgressStorage.Instance == null)
         {
             Debug.Log("CURRENTLEVEL: LevelProgressStorage = null");
@@ -37,20 +35,6 @@ public static class CurrentLevel
 
         LevelProgressStorage.Instance.AddLevelInfo(newLevelData);
         Debug.Log($"CURRENTLEVEL: saved {newLevelData._level}");
-
-      /*  if (LevelProgressStorage.Instance.currentLevels == null)
-        {
-            LevelProgressStorage.Instance.currentLevels = new List<LevelData>();    
-        }
-        _currentLevelIndex = LevelProgressStorage.Instance.currentLevels.FindIndex(item => item._level == newLevelData._level);
-        if (_currentLevelIndex < 0)
-        {
-            Debug.Log($"CURRENTLEVEL: new save for {newLevelData._level} ");
-            LevelProgressStorage.Instance.AddNewLevelInfo(newLevelData);
-        } else {
-            Debug.Log($"CURRENTLEVEL: {newLevelData._level} save is overriten");
-            LevelProgressStorage.Instance.UpdateLevelInfo(newLevelData);
-        } */
 
         SaveLoadManager saveLoadManager = new SaveLoadManager();
         saveLoadManager.SaveGame();
