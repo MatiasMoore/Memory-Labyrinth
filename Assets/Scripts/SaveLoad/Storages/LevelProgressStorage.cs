d482aeba6464cccfa79b8368f13f1564556a301b
andeysiniy <kakavvmm@gmail.com>
2023-11-14 18:19:28 +0300

    refactor(project): made LevelProgressStorage a sealed class

--- Assets/Scripts/SaveLoad/Storages/LevelProgressStorage.cs
+++ Assets/Scripts/SaveLoad/Storages/LevelProgressStorage.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelProgressStorage : MonoBehaviour
+public sealed class LevelProgressStorage : MonoBehaviour
 {
     public static LevelProgressStorage Instance;
 
