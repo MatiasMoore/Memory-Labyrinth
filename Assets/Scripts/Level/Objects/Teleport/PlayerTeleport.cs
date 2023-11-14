aac03063e3a00a18511c09ad0250b7455f04eeba
Maxim Zverkov <75385902+T-LORE@users.noreply.github.com>
2023-11-14 15:37:39 +0300

    feat(project): add teleport event from a true teleporter

--- Assets/Scripts/Level/Objects/Teleport/PlayerTeleport.cs
+++ Assets/Scripts/Level/Objects/Teleport/PlayerTeleport.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerTeleport : TeleportableObject
 {
+    public event UnityAction _teleportEvent;
     public override bool Teleport(Vector3 position)
     {
         GetComponent<MainCharacter>().TeleportTo(position);
+        _teleportEvent?.Invoke();
         return true;
     }
 }
