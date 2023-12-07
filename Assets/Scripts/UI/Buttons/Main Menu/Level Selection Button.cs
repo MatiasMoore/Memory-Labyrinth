3b03b457a4810495dfb8751a842fe78b336c8841
Egor <104233247+RR3Z@users.noreply.github.com>
2023-12-07 16:20:03 +0300

    refactor(project): divided the logic of the MainMenu script into different classes
    Have assigned classes for each of the buttons in the main menu

---
+++ Assets/Scripts/UI/Buttons/Main Menu/Level Selection Button.cs
@@ -0,0 +1,21 @@
+using UnityEngine.Events;
+
+public class LevelSelectionButton : Button
+{
+    public override event UnityAction _buttonClick;
+
+    public override void FireButtonClickAction()
+    {
+        _buttonClick?.Invoke();
+    }
+
+    public override void OnClick()
+    {
+        // Main logic
+        MenuManager.ClosePage(MenuManager.Page.MAIN);
+        MenuManager.OpenPage(MenuManager.Page.LEVEL_SELECTION);
+
+        // Fire events
+        FireButtonClickAction();
+    }
+}
