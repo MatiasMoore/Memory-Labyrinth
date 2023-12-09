13f430634f06636f8986d0e837b605096a7accbd
Egor <104233247+RR3Z@users.noreply.github.com>
2023-12-09 18:39:36 +0300

    feat(project): added a common UnityAction for playing audio when the button is pressed

--- Assets/Scripts/UI/Buttons/Button.cs
+++ Assets/Scripts/UI/Buttons/Button.cs
@@ -3,6 +3,13 @@ using UnityEngine.Events;
 
 public abstract class Button : MonoBehaviour
 {
+    public static event UnityAction _buttonClickSound;
+
+    public static void FireButtonClickSoundAction()
+    {
+        _buttonClickSound?.Invoke();
+    }
+
     public abstract event UnityAction _buttonClick;
 
     public abstract void FireButtonClickAction();
