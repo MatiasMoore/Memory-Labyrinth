de4507955e11667411d2c84b28931e467e444c5a
Maxim Zverkov <75385902+T-LORE@users.noreply.github.com>
2023-12-11 19:03:32 +0300

    refactor(project): add constructor to Finish

--- Assets/Scripts/Level/Objects/Finish point/Finish.cs
+++ Assets/Scripts/Level/Objects/Finish point/Finish.cs
@@ -5,6 +5,10 @@ namespace MemoryLabyrinth.Level.Objects.FinishLib
     [RequireComponent(typeof(BoxCollider2D))]
     public class Finish : MonoBehaviour
     {
+        public Finish()
+        {
+        }
+        
         public void Start()
         {
             GetComponent<BoxCollider2D>().isTrigger = true;
