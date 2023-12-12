ae76b2cd55446ea04a8558ff253aa68d5a825910
MatiasMoore <nikita.birukov.03@mail.ru>
2023-12-12 20:00:01 +0300

    feat(project): method to get achievement by enum

--- Assets/Scripts/Achievements/Logic/AchievementList.cs
+++ Assets/Scripts/Achievements/Logic/AchievementList.cs
@@ -10,5 +10,16 @@ namespace MemoryLabyrinth.Achievemnts
     {
         [SerializeField]
         public List<Achievement> list = new List<Achievement>();
+
+        public Achievement GetAchievementByEnum(Achievement.AchievmentName name)
+        {
+            foreach (Achievement achievement in list)
+            {
+                if (achievement.GetName().Equals(name))
+                    return achievement;
+            }
+
+            return null;
+        }
     }
 }
