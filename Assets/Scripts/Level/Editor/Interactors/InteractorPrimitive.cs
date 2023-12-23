61cf10def415c7024b8d3b61c8f437e4ab01e49a
Maxim Zverkov <75385902+T-LORE@users.noreply.github.com>
2023-12-23 18:20:01 +0300

    feat(project): add interactor primitive

---
+++ Assets/Scripts/Level/Editor/Interactors/InteractorPrimitive.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Level.Editor
+{
+    public abstract class InteractorPrimitive 
+    { 
+        public abstract void InteractAtPos(Vector2 pos);
+    }
+}
+
