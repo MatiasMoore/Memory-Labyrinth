36b619486c494e9818d83daf59603550cb9542a4
MatiasMoore <nikita.birukov.03@mail.ru>
2023-12-24 01:44:21 +0300

    feat(project): added a few functions to ElementCreatorPrimitive

--- Assets/Scripts/Level/Editor/Interactors/ElementCreators/ElementCreatorPrimitive.cs
+++ Assets/Scripts/Level/Editor/Interactors/ElementCreators/ElementCreatorPrimitive.cs
@@ -12,4 +12,27 @@ public class ElementCreatorPrimitive : InteractorPrimitive
         
     }
 
+    public void CreateAsGameObjectAtPos(Vector2 position)
+    {
+        Vector3 posWithDepth = position;
+        posWithDepth.z = _config.zDepth;
+
+        Object.Instantiate(_config.prefab, posWithDepth + _config.offset, Quaternion.identity);
+    }
+
+    public List<GameObject> GetAllObjectsAtPos(Vector2 position)
+    {
+        var colls = Physics2D.OverlapPointAll(position);
+        List<GameObject> objects = new List<GameObject>();
+        foreach (var coll in colls)
+        {
+            objects.Add(coll.gameObject);
+        }
+        return objects;
+    }
+
+    public bool CanBePlacedAtPos(Vector2 position)
+    {
+        return Physics2D.OverlapPoint(position) == null;
+    }
 }
