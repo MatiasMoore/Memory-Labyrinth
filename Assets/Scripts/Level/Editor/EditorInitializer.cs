af3834288d1ac873c546de30c9470474522a104b
Maxim Zverkov <75385902+T-LORE@users.noreply.github.com>
2023-12-23 17:37:13 +0300

    feat(project):  add level editor scene

---
+++ Assets/Scripts/Level/Editor/EditorInitializer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Level.Editor
+{
+    public class EditorInitializer : MonoBehaviour
+    {
+
+        void Start()
+        {
+
+        }
+
+    }
+}
+
