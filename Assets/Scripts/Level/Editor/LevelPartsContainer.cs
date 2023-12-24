527bcd156538a1931cc3a100fc2af3826c39cab4
Maxim Zverkov <75385902+T-LORE@users.noreply.github.com>
2023-12-24 10:06:22 +0300

    feat(project): added "LevelPartType" parameter when creating an element and rewrote container from list to dictionary

--- Assets/Scripts/Level/Editor/LevelPartsContainer.cs
+++ Assets/Scripts/Level/Editor/LevelPartsContainer.cs
@@ -2,31 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.Pool;
 
 namespace Level.Editor
 {
-    public class LevelPartsContainer : MonoBehaviour
+    public class LevelPartsContainer
     {
-        private List<GameObject> _parts = new List<GameObject>();
+        private Dictionary<LevelPartType, List<GameObject>> _parts;
 
-        public void AddPart(GameObject newPart)
+        public List<GameObject> GetPartsByType(LevelPartType type) => _parts[type];
+
+        public LevelPartsContainer()
         {
-            if (_parts.Contains(newPart))
+            _parts = new Dictionary<LevelPartType, List<GameObject>>();
+            foreach (LevelPartType type in LevelPartType.GetValues(typeof(LevelPartType)))
+            {
+                _parts[type] = new List<GameObject>();
+            }
+        }
+
+        public void AddPart(LevelPartType type, GameObject newPart)
+        {
+            if (_parts[type].Contains(newPart))
             {
                 throw new System.Exception($"LevelPartsContainer: {newPart} already exist");
             }
 
-            _parts.Add(newPart);
+            _parts[type].Add(newPart);
         }
 
         public void DeletePart(GameObject part)
         {
-            if (!_parts.Contains(part))
+            bool isRemoved = false;
+
+            foreach (List<GameObject> partList in _parts.Values)
+            {
+                if (partList.Contains(part))
+                {
+                    partList.Remove(part);
+                    isRemoved = true;
+                }
+            }
+
+            if (!isRemoved)
             {
                 throw new System.Exception($"LevelPartsContainer: {part} does not exist");
             }
 
-            _parts.Remove(part);
         }
     }
 }
