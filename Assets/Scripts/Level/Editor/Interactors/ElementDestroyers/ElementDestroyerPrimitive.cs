332f942f6789561ec0315c3a3392575e53b92f4f
Maxim Zverkov <75385902+T-LORE@users.noreply.github.com>
2023-12-24 10:42:18 +0300

    feat(project): add elementDestroyerPrimitive

---
+++ Assets/Scripts/Level/Editor/Interactors/ElementDestroyers/ElementDestroyerPrimitive.cs
@@ -0,0 +1,41 @@
+using Level.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Level.Editor
+{
+    public abstract class ElementDestroyerPrimitive : InteractorPrimitive
+    {
+
+        protected LevelPartsContainer _container;
+
+        public ElementDestroyerPrimitive(LevelPartsContainer container)
+        {
+            _container = container;
+        }
+
+        public void DestroyAtPos(Vector2 pos) {
+            List<GameObject> posObjects = GetAllObjectsAtPos(pos);
+            if (posObjects.Count > 0)
+            {
+                GameObject destroyableObject = posObjects[0];
+                _container.DeletePart(destroyableObject);
+                Object.Destroy(destroyableObject);
+            }
+        }
+
+        public List<GameObject> GetAllObjectsAtPos(Vector2 position)
+        {
+            var colls = Physics2D.OverlapPointAll(position);
+            List<GameObject> objects = new List<GameObject>();
+            foreach (var coll in colls)
+            {
+                objects.Add(coll.gameObject);
+            }
+            return objects;
+        }
+
+    }
+}
+
