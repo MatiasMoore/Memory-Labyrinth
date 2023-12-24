2177ccf92d23dd79f4cdc0d91fd3358a0b2472be
Maxim Zverkov <75385902+T-LORE@users.noreply.github.com>
2023-12-24 10:42:46 +0300

    feat(project): added destroyFirstElement

---
+++ Assets/Scripts/Level/Editor/Interactors/ElementDestroyers/DestroyFirstElement.cs
@@ -0,0 +1,16 @@
+using Level.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyFirstElement : ElementDestroyerPrimitive
+{
+    public DestroyFirstElement(LevelPartsContainer container) : base(container)
+    {
+    }
+
+    public override void InteractAtPos(Vector2 pos)
+    {
+        DestroyAtPos(pos);
+    }
+}
