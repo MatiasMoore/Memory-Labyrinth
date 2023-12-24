a71b2e2e081edab44f230a64a298a4c91a6c8604
Maxim Zverkov <75385902+T-LORE@users.noreply.github.com>
2023-12-24 10:49:50 +0300

    refactor(project): added namespace for LevelEditor

--- Assets/Scripts/Level/Editor/LevelEditor.cs
+++ Assets/Scripts/Level/Editor/LevelEditor.cs
@@ -5,65 +5,69 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class LevelEditor : MonoBehaviour
+namespace Level.Editor
 {
-    private LevelPartsContainer _container;
-    private InteractorPrimitive _interactor;
-    [SerializeField]
-    private LevelParts _levelPartsDataBase;
-
-    public void Init(TouchControls touchControls)
+    public class LevelEditor : MonoBehaviour
     {
-        touchControls.touchDown += OnPlayerTouch;
-        _container = new LevelPartsContainer();
-        StartCreatingLevelPart(LevelPartType.Wall);
+        private LevelPartsContainer _container;
+        private InteractorPrimitive _interactor;
+        [SerializeField]
+        private LevelParts _levelPartsDataBase;
 
-    }
-
-    private void OnPlayerTouch()
-    {
-        Vector2 pos = TouchControls.Instance.getTouchWorldPosition2d();
-        _interactor.InteractAtPos(pos);
-
-    }
-
-    [ContextMenu("Start Creating Wall")]
-    public void StartCreatingWall()
-    {
-        StartCreatingLevelPart(LevelPartType.Wall);
-    }
-
-    public void StartCreatingLevelPart(LevelPartType type)
-    {
-        LevelPartConfig config = _levelPartsDataBase.GetConfigByType(type);
-        switch (type)
+        public void Init(TouchControls touchControls)
         {
-            case LevelPartType.Wall:
-                _interactor = new WallCreator(_container, config);
-                break;
-            case LevelPartType.Path:
-                break;
-            case LevelPartType.Trap:
-                break;
-            case LevelPartType.Checkpoint:
-                break;
-            case LevelPartType.Startpoint:
-                break;
-            case LevelPartType.Finishpoint:
-                break;
-            case LevelPartType.Bonus:
-                break;
-            case LevelPartType.Teleport:
-                break;
-            default:
-                break;
+            touchControls.touchDown += OnPlayerTouch;
+            _container = new LevelPartsContainer();
+            StartCreatingLevelPart(LevelPartType.Wall);
+
         }
-    }
 
-    [ContextMenu("Start Destroy")]
-    public void StartDestroy()
-    {
-        _interactor = new DestroyFirstElement(_container);
-    }
+        private void OnPlayerTouch()
+        {
+            Vector2 pos = TouchControls.Instance.getTouchWorldPosition2d();
+            _interactor.InteractAtPos(pos);
 
+        }
+
+        [ContextMenu("Start Creating Wall")]
+        public void StartCreatingWall()
+        {
+            StartCreatingLevelPart(LevelPartType.Wall);
+        }
+
+        public void StartCreatingLevelPart(LevelPartType type)
+        {
+            LevelPartConfig config = _levelPartsDataBase.GetConfigByType(type);
+            switch (type)
+            {
+                case LevelPartType.Wall:
+                    _interactor = new WallCreator(_container, config);
+                    break;
+                case LevelPartType.Path:
+                    break;
+                case LevelPartType.Trap:
+                    break;
+                case LevelPartType.Checkpoint:
+                    break;
+                case LevelPartType.Startpoint:
+                    break;
+                case LevelPartType.Finishpoint:
+                    break;
+                case LevelPartType.Bonus:
+                    break;
+                case LevelPartType.Teleport:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        [ContextMenu("Start Destroy")]
+        public void StartDestroy()
+        {
+            _interactor = new DestroyFirstElement(_container);
+        }
+
+    }
 }
+
