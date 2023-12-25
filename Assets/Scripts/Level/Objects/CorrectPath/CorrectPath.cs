e41a1e6f7f2686807756a1558b545d1274425667
Maxim Zverkov <75385902+T-LORE@users.noreply.github.com>
2023-12-25 15:52:14 +0300

    feat(project): add correct path element

---
+++ Assets/Scripts/Level/Objects/CorrectPath/CorrectPath.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MemoryLabyrinth.Level.Objects.CorrectPathLib
+{
+    public class CorrectPath : MonoBehaviour
+    {
+
+    }
+}
+
