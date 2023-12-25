849436512782aa7f269a7b0ccf9da9fbefa04cc0
Maxim Zverkov <75385902+T-LORE@users.noreply.github.com>
2023-12-25 18:55:13 +0300

    feat(project): now paths can be placed on walls

--- Assets/Scripts/Level/Editor/Interactors/ElementCreators/PathCreator.cs
+++ Assets/Scripts/Level/Editor/Interactors/ElementCreators/PathCreator.cs
@@ -1,6 +1,15 @@
+using MemoryLabyrinth.Level.Objects.BonusLib;
+using MemoryLabyrinth.Level.Objects.CheckpointLib;
+using MemoryLabyrinth.Level.Objects.FinishLib;
+using MemoryLabyrinth.Level.Objects.PathLib;
+using MemoryLabyrinth.Level.Objects.StartpointLib;
+using MemoryLabyrinth.Level.Objects.TeleportLib;
+using MemoryLabyrinth.Level.Objects.Trap;
+using MemoryLabyrinth.Level.Objects.WallLib;
 using MemoryLabyrinth.Utils;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 namespace MemoryLabyrinth.Level.Editor
@@ -9,6 +18,19 @@ namespace MemoryLabyrinth.Level.Editor
     {
         public PathCreator(LevelPartsContainer container, LevelPartConfig config) : base(container, config)
         {
+            objectPlaced += DeleteWall;
+        }
+
+        private void DeleteWall(GameObject obj)
+        {
+            foreach (var element in _container.GetObjectsAtPos(obj.transform.position))
+            {
+                if (element.GetComponent<Wall>() != null)
+                {
+                    _container.DeletePart(element);
+                    Object.Destroy(element);
+                }
+            }
         }
 
         public override bool CanBePlacedAtPos(Vector2 position)
@@ -16,8 +38,9 @@ namespace MemoryLabyrinth.Level.Editor
             var objsAtPos = _container.GetObjectsAtPos(position);
 
             bool noObjects = objsAtPos.Count == 0;
-            
-            return noObjects;
+            bool isWallPresent = _container.ContainsPartAtPos<Wall>(position);
+          
+            return noObjects || isWallPresent;
         }
     }
 }
